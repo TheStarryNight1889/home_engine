80d35b9c957679f2bd211fcb235c306ccdd51d4e
TheStarryNight1889 <30705821+TheStarryNight1889@users.noreply.github.com>
2022-11-25 01:10:05 +0000

    added scss support (still kinda borked though)

---
+++ apps/home-engine-app/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<div>
+    <div class="sidenav">
+        <div class="sidenav-header">
+            <div class="sidenav-header-title">
+                <h1>Home Engine</h1>
+            </div>
+        </div>
+        <div class="sidenav-body">
+            <div class="sidenav-body-item">
+                <a href="/dashboard">Dashboard</a>
+            </div>
+            <div class="sidenav-body-item">
+                <a href="/devices">Devices</a>
+            </div>
+            <div class="sidenav-body-item">
+                <a href="/rooms">Rooms</a>
+            </div>
+            <div class="sidenav-body-item">
+                <a href="/settings">Setgs</a>
+            </div>
+        </div>
+    </div>
+</div>
+<slot></slot>
+
+<style lang="scss">
+
+</style>
