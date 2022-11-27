edd91c836bf9be4fae55c8514927fcaad5615dc4
TheStarryNight1889 <30705821+TheStarryNight1889@users.noreply.github.com>
2022-11-27 21:26:25 +0000

    layout

--- apps/home-engine-app/src/routes/+layout.svelte
+++ apps/home-engine-app/src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<div>
+<div class="layout">
     <div class="sidenav">
         <div class="sidenav-header">
             <div class="sidenav-header-title">
@@ -16,13 +16,39 @@
                 <a href="/rooms">Rooms</a>
             </div>
             <div class="sidenav-body-item">
-                <a href="/settings">Setgs</a>
+                <a href="/settings">Settings</a>
             </div>
         </div>
     </div>
+    <div class="content">
+        <slot></slot>
+    </div>
 </div>
-<slot></slot>
 
 <style lang="scss">
-
+    .content{
+        width: 100%;
+    }
+    .layout {
+        display: flex;
+        flex-direction: row;
+        height: 100vh;
+        width: 100vw;
+    }
+    .sidenav{
+        max-width: 10vh;
+        min-width: 10vh;
+        color: white;
+        height: 100vh;
+        background-color: teal;
+        padding: 1rem;
+        .sidenav-body{
+            .sidenav-body-item {
+                a {
+                    text-decoration: none;
+                    color: white;
+                }
+            }
+        }
+    }
 </style>
