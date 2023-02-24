8a0068c1260b4eb5142b08f0be63346a2956d9f8
TheStarryNight1889 <30705821+TheStarryNight1889@users.noreply.github.com>
2023-02-24 23:59:02 +0000

    added web placeholder

--- apps/website/src/routes/+page.svelte
+++ apps/website/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-screen">
+	<div class="bg-sky-50 text-center p-24 h-1/3">
+		<h1 class="text-8xl text-cyan-500 drop-shadow-sm">Saer</h1>
+		<p class="text-xl text-cyan-400 drop-shadow-xs">
+			Monitor your air quality with state of the art IoT devices.
+		</p>
+	</div>
+	<div
+		class="text-center flex flex-col space-y-12 justify-center items-center h-2/3 mx-auto my-auto"
+	>
+		<h1 class="text-6xl">Coming soon...</h1>
+		<a href="https://github.com/TheStarryNight1889/home_engine" target="_blank" rel="noreferrer">
+			<button
+				class="bg-sky-200 p-2 rounded-md border border-cyan-300 flex justify-center items-center hover:bg-cyan-300"
+			>
+				<img src="./github-mark.svg" alt="" class="w-6 mr-2" />
+				GitHub
+			</button>
+		</a>
+	</div>
+</div>
