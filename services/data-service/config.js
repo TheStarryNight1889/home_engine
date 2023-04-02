6981897dd3446aa80f9781a1a01249e65e4831b5
TheStarryNight1889 <30705821+TheStarryNight1889@users.noreply.github.com>
2023-04-02 22:26:25 +0100

    d Please enter the commit message for your changes. Lines starting

--- services/data-service/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    influxUrl: process.env.INFLUX_URL || 'https://eu-central-1-1.aws.cloud2.influxdata.com',
-    influxOrg: process.env.INFLUX_ORG || 'molloy.christie@gmail.com',
-    influxToken: process.env.INFLUX_TOKEN || 'NiceTryHacker',
-    influxSensorBucket: process.env.INFLUX_SENSOR_BUCKET || 'Sensor',
-    wsPort: process.env.WS_PORT || 8080,
-}
