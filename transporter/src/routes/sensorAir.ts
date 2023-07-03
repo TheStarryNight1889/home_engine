38c8f329b831eb9ea1faead38ad4535f382ce4cc
TheStarryNight1889 <30705821+TheStarryNight1889@users.noreply.github.com>
2023-07-03 18:59:27 +0100

    added support for deviceId when getting air data

--- transporter/src/routes/sensorAir.ts
+++ transporter/src/routes/sensorAir.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { Air } from '../models/air';
 import { Wss } from '../servers/wss';
-import { Sequelize, Op } from 'sequelize';
+import { Sequelize, Op, FindOptions } from 'sequelize';
 
 const router = express.Router();
 
@@ -28,38 +28,46 @@ router.post('/sensor/air', async (req, res) => {
 });
 router.get('/sensor/air', async (req, res) => {
     try {
-        const data = await Air.findAll({
-            attributes: [
-              [
-                Sequelize.literal("time_bucket('5 minutes', time)"),
-                'time_bucket',
-              ],
-              [
-                Sequelize.literal("ROUND(AVG(temperature))"),
-                'temperature',
-            ],
-            [
-                Sequelize.literal("ROUND(AVG(humidity))"),
-                'humidity',
-            ],
-            [
-                Sequelize.literal("ROUND(AVG(co2))"),
-                'co2',
-            ],
-            ],
-            where: {
-              time: {
-                [Op.gte]: Sequelize.literal("NOW() - INTERVAL '7 days'"),
-              },
-            },
-            group: ['time_bucket'],
-            order: [['time_bucket', 'ASC']],
-          });
+        if (!req.query.deviceId) {
+            res.status(400).send("deviceId is required");
+            return;
+        }
+        const deviceId: string = req.query.deviceId as string;
+        const data = await Air.findAll(fiveMinuteAggregationByDevice(deviceId));
         res.send(data);
     } catch (err) {
         console.error(err);
         res.sendStatus(500);
     }
+});
+
+const fiveMinuteAggregationByDevice: (deviceId: string) => FindOptions<any> = (deviceId: string) => ({
+  attributes: [
+    [
+      Sequelize.literal("time_bucket('5 minutes', time)"),
+      'time_bucket',
+    ],
+    [
+      Sequelize.literal("ROUND(AVG(temperature))"),
+      'temperature',
+    ],
+    [
+      Sequelize.literal("ROUND(AVG(humidity))"),
+      'humidity',
+    ],
+    [
+      Sequelize.literal("ROUND(AVG(co2))"),
+      'co2',
+    ],
+  ],
+  where: {
+    device_id: deviceId,
+    time: {
+      [Op.gte]: Sequelize.literal("NOW() - INTERVAL '7 days'"),
+    },
+  },
+  group: ['time_bucket'],
+  order: [['time_bucket', 'ASC']],
 });
 
 export { router as sensorAirRouter };
