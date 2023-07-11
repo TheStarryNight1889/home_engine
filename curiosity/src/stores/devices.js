ac9e25b755a15d5cdf738b22df36abdc7fca0827
TheStarryNight1889 <30705821+TheStarryNight1889@users.noreply.github.com>
2023-07-11 23:18:09 +0100

    frontend will now listen to changes in connection info

--- curiosity/src/stores/devices.js
+++ curiosity/src/stores/devices.js
@@ -15,6 +15,15 @@ export const useDevicesStore = defineStore('devices', () => {
     connectionInfo.value = await getDeviceConnections()
   }
 
+  const updateConnectionInfo = (data) => {
+    const index = connectionInfo.value.findIndex(connection => connection.device_id === data.device_id)
+    if(index === -1){
+      connectionInfo.value.push(data)
+    } else{
+      connectionInfo.value[index] = data
+    }
+  }
+
   // getters
   const getDeviceById = (id) => {
     return all.value.find(device => device.device_id === id)
@@ -24,5 +33,5 @@ export const useDevicesStore = defineStore('devices', () => {
     return connectionInfo.value.find(connection => connection.device_id === deviceId)
   }
 
-  return { all, setAll, setConnectionInfo, getDeviceById, getConnectionInfoByDeviceId }
+  return { all, setAll, setConnectionInfo, getDeviceById, getConnectionInfoByDeviceId, updateConnectionInfo }
 })
--- curiosity/src/ws/useWs.js
+++ curiosity/src/ws/useWs.js
@@ -1,11 +1,13 @@
 // useWebSocket.js
 import { ref, watchEffect } from 'vue'
 import { useAirsStore } from '@/stores/airs'
+import { useDevicesStore } from '@/stores/devices'
 
 export function useWs(url) {
   const socket = ref(null)
   const error = ref(null)
   const airStore = useAirsStore();
+  const deviceStore = useDevicesStore();
 
   watchEffect(() => {
     socket.value = new WebSocket(url)
@@ -13,9 +15,10 @@ export function useWs(url) {
     socket.value.onmessage = (event) => {
       const message = JSON.parse(event.data)
 
-      // Dispatch a Vuex action or commit a mutation depending on the message type
       if (message.type === 'air') {
         airStore.setLatest(message.data)
+      } else if(message.type === 'deviceConnection'){
+        deviceStore.updateConnectionInfo(message.data)
       } else{
         console.log(`[useWs] message type not found: ${message.type}`)
       }
