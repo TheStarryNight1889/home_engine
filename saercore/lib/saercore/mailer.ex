6bb71ac140430d0d2d8fb0c8fa3cd991ee55fc28
TheStarryNight1889 <30705821+TheStarryNight1889@users.noreply.github.com>
2025-01-18 23:24:06 +0000

    if i decide to rewrite this one more time...

--- saercore/lib/saercore/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule Saercore.Mailer do
-  use Swoosh.Mailer, otp_app: :saercore
-end
--- saercore/lib/saercore/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Saercore.Repo do
-  use Ecto.Repo,
-    otp_app: :saercore,
-    adapter: Ecto.Adapters.Postgres
-end
