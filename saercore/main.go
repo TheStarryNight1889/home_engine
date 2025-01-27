69b97317ae92af89db5035aee928f8510d8c08a0
TheStarryNight1889 <30705821+TheStarryNight1889@users.noreply.github.com>
2025-01-27 21:29:31 +0000

    adding mqtt functionality

--- saercore/main.go
+++ saercore/main.go
@@ -13,37 +13,56 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func main() {
+func loadEnvVars() {
 	if err := config.Load(); err != nil {
 		log.Fatal("Could not load env file")
 	}
-	dbConn := os.Getenv("DB_CONN")
-	serverPort := os.Getenv("SERVER_PORT")
+}
 
+func startDbPool() *pgxpool.Pool {
+	dbConnectionString := os.Getenv("DB_CONN")
 	ctx := context.Background()
 
-	dbpool, err := pgxpool.New(ctx, dbConn)
+	dbpool, err := pgxpool.New(ctx, dbConnectionString)
 	if err != nil {
 		log.Println(err)
 		log.Fatal("Could not connect to the database")
 	}
-	defer dbpool.Close()
+	return dbpool
+}
 
-	queries := db.New(dbpool)
-
-	deviceHandler := handler.NewDeviceHandler(queries)
-	locationHandler := handler.NewLocationHandler(queries)
-	sensorAirHandler := handler.NewSensorAirHandler(queries)
+func createMux(querier *db.Queries) *http.ServeMux {
+	deviceHandler := handler.NewDeviceHandler(querier)
+	locationHandler := handler.NewLocationHandler(querier)
+	sensorAirHandler := handler.NewSensorAirHandler(querier)
 
 	mux := http.NewServeMux()
 	deviceHandler.RegisterRoutes(mux)
 	locationHandler.RegisterRoutes(mux)
 	sensorAirHandler.RegisterRoutes(mux)
 
+	return mux
+}
+
+func startHttpServer(mux *http.ServeMux) {
+	serverPort := os.Getenv("SERVER_PORT")
 	server := &http.Server{
 		Addr:    ":" + serverPort,
 		Handler: middleware.Use(mux, middleware.Logger, middleware.Json),
 	}
 
 	log.Fatal(server.ListenAndServe())
+
+}
+
+func main() {
+	loadEnvVars()
+
+	dbPool := startDbPool()
+	defer dbPool.Close()
+
+	querier := db.New(dbPool)
+	mux := createMux(querier)
+
+	startHttpServer(mux)
 }
---
+++ saercore/mqtt/client.go
@@ -0,0 +1,109 @@
+package mqtt
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/url"
+	"os"
+
+	"github.com/eclipse/paho.golang/autopaho"
+	"github.com/eclipse/paho.golang/paho"
+)
+
+func getUrl() *url.URL {
+	const mqttUrlStr = "MQTT_URL"
+	mqttUrl := os.Getenv(mqttUrlStr)
+	url, err := url.Parse(mqttUrl)
+	if err != nil {
+		log.Fatal("Could not parse" + mqttUrlStr)
+	}
+	return url
+}
+
+func getClientID() string {
+	const clientIdStr = "MQTT_CLIENT_ID"
+	clientId := os.Getenv(clientIdStr)
+	return clientId
+
+}
+
+type MqttClient struct {
+	ctx          context.Context
+	client       *autopaho.ConnectionManager
+	clientConfig autopaho.ClientConfig
+}
+
+func NewMqttClient() *MqttClient {
+	ctx := context.Background()
+	u := getUrl()
+	clientConfig := autopaho.ClientConfig{
+		ServerUrls:                    []*url.URL{u},
+		KeepAlive:                     20,
+		CleanStartOnInitialConnection: false,
+		SessionExpiryInterval:         60,
+		OnConnectionUp: func(cm *autopaho.ConnectionManager, c *paho.Connack) {
+			log.Println("Connected to MQTT Broker")
+			if _, err := cm.Subscribe(ctx, &paho.Subscribe{
+				Subscriptions: []paho.SubscribeOptions{
+					{Topic: "test/test", QoS: 1},
+				},
+			}); err != nil {
+				log.Fatal("failed to subscribe to topics. exiting...")
+			}
+			log.Println("subscribed to topics")
+		},
+		OnConnectError: func(err error) { log.Fatal("error whilst attempting connection") },
+		ClientConfig: paho.ClientConfig{
+			ClientID: getClientID(),
+			OnPublishReceived: []func(paho.PublishReceived) (bool, error){
+				func(pr paho.PublishReceived) (bool, error) {
+					log.Printf("received message on topic %s; body: %s (retain: %t)\n", pr.Packet.Topic, pr.Packet.Payload, pr.Packet.Retain)
+					return true, nil
+				}},
+			OnClientError: func(err error) { log.Printf("client error: %s\n", err) },
+			OnServerDisconnect: func(d *paho.Disconnect) {
+				if d.Properties != nil {
+					log.Printf("server requested disconnect: %s\n", d.Properties.ReasonString)
+				} else {
+					log.Printf("server requested disconnect; reason code: %d\n", d.ReasonCode)
+				}
+			},
+		},
+	}
+
+	client, err := autopaho.NewConnection(ctx, clientConfig)
+	if err != nil {
+		panic(err)
+	}
+	if err = client.AwaitConnection(ctx); err != nil {
+		panic(err)
+	}
+
+	return &MqttClient{
+		client:       client,
+		clientConfig: clientConfig,
+		ctx:          ctx,
+	}
+}
+
+func (mc *MqttClient) Publish(qos int8, topic string, payload any) error {
+	bytes, err := json.Marshal(payload)
+	ctx := context.Background()
+	if err != nil {
+		return err
+	}
+
+	if _, err = mc.client.Publish(context.Background(), &paho.Publish{
+		QoS:     1,
+		Topic:   topic,
+		Payload: bytes,
+	}); err != nil {
+		if ctx.Err() == nil {
+			panic(err) // Publish will exit when context cancelled or if something went wrong
+		}
+	}
+
+	ctx.Done()
+	return nil
+}
